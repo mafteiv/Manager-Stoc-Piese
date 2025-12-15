8c90449f40151e1986014defcd2e418dea9a706e
mafteiv <91034926+mafteiv@users.noreply.github.com>
2025-12-15 14:47:11 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important pentru GitHub Pages
+  base: './', // Asigură că toate link-urile din build sunt relative, esențial pentru GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
