69e5a9d603f42de86f772f993f9b5768745b8898
MafteiVasile <mafteiv@yahoo.com>
2025-12-15 23:29:19 +0200

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asigură că toate link-urile din build sunt relative, esențial pentru GitHub Pages
+  base: '/Manager-Stoc-Piese/', // Pentru GitHub Pages: /<nume-repository>/
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
